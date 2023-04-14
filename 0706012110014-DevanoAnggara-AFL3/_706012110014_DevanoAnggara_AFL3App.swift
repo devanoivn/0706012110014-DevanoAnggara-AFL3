5ed785053df762bba345a5ca95dd5b37ab5f3d34
Devano <danggara@student.ciputra.ac.id>
2023-04-14 08:45:41 +0700

    Initial Commit

---
+++ 0706012110014-DevanoAnggara-AFL3/_706012110014_DevanoAnggara_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110014_DevanoAnggara_AFL3App.swift
+//  0706012110014-DevanoAnggara-AFL3
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110014_DevanoAnggara_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
