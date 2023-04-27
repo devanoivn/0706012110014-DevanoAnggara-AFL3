5d80f33080b248129c9e1e90a6b2801a3ced3d8e
Devano <danggara@student.ciputra.ac.id>
2023-04-27 19:57:04 +0700

    Hi

--- 0706012110014-DevanoAnggara-AFL3/Model/Landamark.swift
+++ 0706012110014-DevanoAnggara-AFL3/Model/Landamark.swift
@@ -4,11 +4,9 @@
 //
 //  Created by MacBook Pro on 14/04/23.
 //
-
 import Foundation
 import SwiftUI
 import CoreLocation
-
 
 struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
@@ -17,18 +15,30 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
-    
+    var isFeatured: Bool
+
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
+    var featureImage: Image? {
+            isFeatured ? Image(imageName + "_feature") : nil
+        }
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
-    
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
